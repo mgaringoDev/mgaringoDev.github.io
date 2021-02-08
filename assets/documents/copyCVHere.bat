cc3fedf7b698e6a09073a5f5ffd2dfeb914bf8a8
mgaringoDev <mg.mydev@gmail.com>
2021-02-08 16:41:03 -0500

    Auto Update
    Added the functionality of automatically updating the CV using a .bat
    file

---
+++ assets/documents/copyCVHere.bat
@@ -0,0 +1,8 @@
+@ECHO OFF
+
+copy /y C:\Cerebral\_MarioSandBox\Website\newMgaringoDevGithub\mgaringoDev.github.io\assets\documents\newCV\cv.pdf C:\Cerebral\_MarioSandBox\Website\newMgaringoDevGithub\mgaringoDev.github.io\assets\documents\mycv.pdf
+
+copy /y C:\Cerebral\_MarioSandBox\Website\newMgaringoDevGithub\mgaringoDev.github.io\assets\documents\newCV\cv.pdf C:\Cerebral\_MarioSandBox\Website\newMgaringoDevGithub\mgaringoDev.github.io\assets\documents\MarioGaringoCV.pdf
+
+
+
